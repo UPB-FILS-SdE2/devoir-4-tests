f3f0260815fc743e21a13ecac02c9c639096f6f4
Ioana Culic <ioana_mculic@yahoo.com>
2021-04-10 20:20:50 +0300

    added tests

---
+++ run.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+dir="$(dirname $0)"
+main="$dir/main.py"
+
+function run_test {
+    rm -rf output
+    inputfile="$dir/verify/$1"
+    outputfile="$(dirname $inputfile)/$(basename $1 .pse).$2.ref"
+    echo $outputfile
+    echo Running $inputfile
+    python3 $main $2 3 $inputfile > output
+    ERROR=0
+    if ! diff output $outputfile -b --side-by-side > error;
+    then
+        echo "Your output                                           | Correct output"
+        cat error
+        ERROR=1
+    else
+        echo Correct
+    fi
+    rm -rf output
+    rm -rf error
+    return $ERROR
+}
+
+if [ $# -lt 1 ];
+then
+    echo "Not enough parameters"
+else
+    run_test $1 $2
+fi 
